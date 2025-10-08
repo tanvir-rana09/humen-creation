36513aa3ca5d380d3e6d457ea8118641c489c1eb
tanvir-rana09 <tanvir.rana.soikat@gmail.com>
2025-10-08 21:48:40 +0600

    sdf

--- src/components/FamilyStoryCard.jsx
+++ src/components/FamilyStoryCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const FamilyStoryCard = ({img}) => {
 	return (
-				<div className='rounded-[35px] shadow-xl border-[3px] parent border-gray-600 cursor-pointer hover: transition-all duration-500 has-[.child:hover]:-rotate-8 w-[23rem]  xl:w-[28rem]'>
-			<div className="relative z-10 bg-white rounded-[32px] hover:rotate-0 child -rotate-8  p-2.5 shadow-xl border-[3px] border-gray-600  transition-all duration-500 aspect-5/5 w-[23rem]  xl:w-[28rem]">
+				<div className='rounded-[35px] shadow-xl border-[3px] parent border-gray-600 cursor-pointer hover: transition-all duration-500 has-[.child:hover]:-rotate-8 xl:w-[28rem]'>
+			<div className="relative z-10 bg-[#FFFCEA] rounded-[32px] hover:rotate-0 child -rotate-8  p-2.5 shadow-xl border-[3px] border-gray-600  transition-all duration-500 aspect-5/5 xl:w-[28rem]">
 				<div style={{
 					backgroundImage: `url(${img})`,
 					backgroundSize: 'cover',
